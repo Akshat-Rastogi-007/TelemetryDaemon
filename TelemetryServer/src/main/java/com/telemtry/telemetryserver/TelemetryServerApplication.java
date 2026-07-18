f675f1ed1d954dc87f6e517b95a4b52ef7bd28a6
Akshat Rastogi <akshatrastogi.jaipuriahardoi@gmail.com>
2026-07-18 22:46:44 +0530

    Server Structure Initialized

---
+++ TelemetryServer/src/main/java/com/telemtry/telemetryserver/TelemetryServerApplication.java
@@ -0,0 +1,13 @@
+package com.telemtry.telemetryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemetryServerApplication.class, args);
+    }
+
+}
